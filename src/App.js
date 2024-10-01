0c8fcf2c76355aed7b19f1b24923daa65d37b302
Karina1014 <janneth_wi15@hotmail.com>
2024-09-30 19:02:37 -0500

    💻 Contenedor para Testimonios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bienvenidos a mi Aplicación</h1> {/* Agregar un encabezado para mayor claridad */}
+      <Testimonio/>
     </div>
   );
 }
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-function Testimonio(){
-    return(
-			<div className='contenedor-testimonio'>
-
-			</div>
-		);
+function Testimonio() {
+    return (
+        <div className='contenedor-testimonio'>
+            <img 
+                className='imagen-testimonio' // Clase opcional para estilos
+								src={require('../imagenes/karina.png') }// Reemplaza con la ruta de tu imagen
+                alt='foto de karina' />
+            <h3 className='nombre-testimonio'>Nombre del Testigo</h3>
+            <p className='texto-testimonio'>
+                "Aquí va el contenido del testimonio. Una breve descripción de la experiencia que se quiere compartir."
+            </p>
+        </div>
+    );
 }
+
+export default Testimonio;
